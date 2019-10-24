97a057aaea73e8b49a71d35332166e2acbb3d412
BossierCrane <55777802+BossierCrane@users.noreply.github.com>
2019-10-24 13:39:58 +1000

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,6 +1,46 @@
 const OBSWebSocket = require('obs-websocket-js');
 const obs = new OBSWebSocket();
 
+const scenes = [
+    {
+        scene: 'Start Scene',
+        paths: ['Scene 2-1', 'Scene 2-2']
+    },
+    {
+        scene: 'Scene 2-1',
+        paths: ['Scene 3-1', 'Scene 3-2-1']
+    },
+    {
+        scene: 'Scene 2-2',
+        paths: ['Scene 3-2-2', 'Scene 3-3']
+    },
+    {
+        scene: 'Scene 3-1',
+        paths: ['Scene 3-2-3', 'Scene 4-1']
+    },
+    {
+        scene: 'Scene 3-2-1',
+        paths: ['Scene 4-2', 'Scene4-3']
+    },
+    {
+        scene: 'Scene 3-2-2',
+        paths: ['Scene 4-2', 'Scene4-3']
+    },
+    {
+        scene: 'Scene 3-2-3',
+        paths: ['Scene 4-2', 'Scene4-3']
+    },
+    {
+        scene: 'Scene 3-3',
+        paths: ['Scene 4-2', 'Scene 4-3']
+    }
+    
+]
+
+const start = scenes[0]; 
+
+const scene21 = scenes.find((s) => s.scene === 'Scene 2-1')
+
 init().then(() => {
     // ... event handlers ..
     handleInput();
@@ -44,4 +84,3 @@ async function handleInput() {
         }
     }
 }
-// SetCurrentScene: { "scene-name": string };
